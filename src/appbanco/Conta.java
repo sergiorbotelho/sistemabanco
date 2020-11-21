e407f93a1d2dfe1adcfcf49e73d2ed4b6584ae21
Sergio Botelho <sergiorbotelho@gmail.com>
2020-11-21 12:13:07 -0300

    Atualização.

--- src/appbanco/Conta.java
+++ src/appbanco/Conta.java
@@ -2,56 +2,54 @@ package appbanco;
 
 public class Conta {
 
-	 protected int num;
+	protected int num;
 	protected double saldo;
+
 	public Conta(int num, double saldo) {
-	super();
-	this.num = num;
-	this.saldo = saldo;
+		super();
+		this.num = num;
+		this.saldo = saldo;
 	}
 
-	 public Conta() {
+	public Conta() {
 	}
+
 	public int getNum() {
-	return num;
+		return num;
 	}
 
-	 public void setNum( int n) {
-	num = n;
+	public void setNum(int n) {
+		num = n;
 	}
 
-	 public double getSaldo() {
-	return saldo;
-	}
-	public void setSaldo( double s) {
-	saldo = s;
+	public double getSaldo() {
+		return saldo;
 	}
 
-	public void debite( double val )
-	{
-	if (val<= saldo)
-	{
-	saldo = saldo - val;
+	public void setSaldo(double s) {
+		saldo = s;
 	}
-	else
-	System.out.println("Saldo insuficiente!");
+
+	public void debite(double val) {
+		if (val <= saldo) {
+			saldo = saldo - val;
+		} else
+			System.out.println("Saldo insuficiente!");
 	}
-	public void credite( double val )
-	{
-	saldo = saldo + val;
+
+	public void credite(double val) {
+		saldo = saldo + val;
 	}
-	public void exibeConta()
-	{
-	System.out.println("-------------------------");
-	System.out.println("Número: "+num);
-	System.out.println("Saldo : "+saldo);
-	System.out.println("=========================");
+
+	public void exibeConta() {
+		System.out.println("-------------------------");
+		System.out.println("Número: " + num);
+		System.out.println("Saldo : " + saldo);
+		System.out.println("=========================");
 	}
-	public String toString()
-	{
-	return "-------------------------\n" +
-	"Número: "+num+"\n" +
-	"Saldo : "+saldo+"\n" +
-	"=========================";
+
+	public String toString() {
+		return "-------------------------\n" + "Número: " + num + "\n" + "Saldo : " + saldo + "\n"
+				+ "=========================";
 	}
-	}
+}
