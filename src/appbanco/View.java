56d8b7aa927fe582d0a67be91082fb271d2f25f3
Sergio Botelho <sergiorbotelho@gmail.com>
2020-11-21 03:29:24 -0300

    Layout

--- src/appbanco/View.java
+++ src/appbanco/View.java
@@ -11,6 +11,14 @@ import javax.swing.JMenuItem;
 import javax.swing.JMenuBar;
 import javax.swing.SwingConstants;
 import java.awt.Component;
+import java.awt.Toolkit;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.KeyStroke;
+import java.awt.event.KeyEvent;
+import java.awt.event.InputEvent;
+import java.awt.Color;
 
 public class View extends JFrame {
 
@@ -36,6 +44,8 @@ public class View extends JFrame {
 	 * Create the frame.
 	 */
 	public View() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\S\u00E9rgio Botelho Filho\\Pictures\\favicon_io123\\android-chrome-512x512.png"));
+		setTitle("Banco Enterprise");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		
@@ -44,17 +54,27 @@ public class View extends JFrame {
 		setJMenuBar(menuBar);
 		
 		JMenu mnNewMenu_1 = new JMenu("Ajuda");
-		mnNewMenu_1.setHorizontalAlignment(SwingConstants.CENTER);
+		mnNewMenu_1.setHorizontalAlignment(SwingConstants.RIGHT);
 		menuBar.add(mnNewMenu_1);
-		
-		JMenuItem mntmNewMenuItem = new JMenuItem("Manual");
-		mnNewMenu_1.add(mntmNewMenuItem);
-		
-		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Sobre");
-		mnNewMenu_1.add(mntmNewMenuItem_1);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setBackground(Color.WHITE);
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\S\u00E9rgio Botelho Filho\\eclipse-workspace\\SistemaBanco\\img\\png.png"));
+		lblNewLabel.setBounds(0, 0, 434, 198);
+		contentPane.add(lblNewLabel);
+		
+		JButton btnNewButton_1 = new JButton("Login");
+		btnNewButton_1.setBackground(Color.WHITE);
+		btnNewButton_1.setBounds(246, 209, 89, 23);
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton = new JButton("Cadastro");
+		btnNewButton.setBounds(94, 209, 89, 23);
+		contentPane.add(btnNewButton);
 	}
 }
