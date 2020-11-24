681f5c8a72fc081332dde7d494d68576538c7189
Sergio Botelho <sergiorbotelho@gmail.com>
2020-11-23 22:51:51 -0300

    Atualizado

--- src/appbanco/View.java
+++ src/appbanco/View.java
@@ -8,13 +8,27 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JMenuBar;
 import javax.swing.SwingConstants;
 import java.awt.Component;
-import javax.swing.Box;
+import java.awt.Toolkit;
 import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.KeyStroke;
+import java.awt.event.KeyEvent;
+import java.awt.event.InputEvent;
+import java.awt.Color;
+import javax.swing.AbstractAction;
+import java.awt.event.ActionEvent;
+import javax.swing.Action;
+import java.awt.event.ActionListener;
 
 public class View extends JFrame {
+
+	private JPanel contentPane;
+	private final Action action = new SwingAction();
 
 	/**
 	 * Launch the application.
@@ -36,8 +50,73 @@ public class View extends JFrame {
 	 * Create the frame.
 	 */
 	public View() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\S\u00E9rgio Botelho Filho\\Pictures\\favicon_io123\\android-chrome-512x512.png"));
+		setTitle("Banco Enterprise");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 521, 309);
-		getContentPane().setLayout(null);
+		setBounds(100, 100, 450, 300);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setAlignmentY(Component.CENTER_ALIGNMENT);
+		setJMenuBar(menuBar);
+		
+		JMenu mnNewMenu_1 = new JMenu("Ajuda");
+		mnNewMenu_1.setHorizontalAlignment(SwingConstants.RIGHT);
+		menuBar.add(mnNewMenu_1);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("Manual");
+		mnNewMenu_1.add(mntmNewMenuItem);
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Sobre");
+		mntmNewMenuItem_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "Eu disse pra cliclar abaixo seu animal");
+			}
+		});
+		mnNewMenu_1.add(mntmNewMenuItem_1);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton_1 = new JButton("Login");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			@SuppressWarnings("deprecation")
+			public void actionPerformed(ActionEvent e) {
+				ViewLogin viewlogin = new ViewLogin();
+				viewlogin.show();
+				dispose();
+			}
+		});
+		
+		JButton btnNewButton = new JButton("Cadastro");
+		btnNewButton.addActionListener(new ActionListener() {
+			@SuppressWarnings("deprecation")
+			public void actionPerformed(ActionEvent e) {
+				ViewCadastro viewCadastro = new ViewCadastro();
+				viewCadastro.show();
+				dispose();
+						
+			}
+		});
+		btnNewButton.setBounds(103, 168, 89, 23);
+		contentPane.add(btnNewButton);
+		btnNewButton_1.setBackground(Color.WHITE);
+		btnNewButton_1.setBounds(255, 168, 89, 23);
+		contentPane.add(btnNewButton_1);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setBackground(Color.WHITE);
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\S\u00E9rgio Botelho Filho\\eclipse-workspace\\SistemaBanco\\img\\png.png"));
+		lblNewLabel.setBounds(0, 0, 434, 239);
+		contentPane.add(lblNewLabel);
+	}
+	private class SwingAction extends AbstractAction {
+		public SwingAction() {
+			putValue(NAME, "SwingAction");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
 	}
 }
